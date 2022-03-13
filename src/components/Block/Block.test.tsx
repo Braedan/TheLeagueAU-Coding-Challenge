df0027febf8983455ca82237b63b36fd895336ce
Braedan <Braedan@users.noreply.github.com>
2022-03-13 15:50:29 +0800

    Removed unused imports

--- src/components/Block/Block.test.tsx
+++ src/components/Block/Block.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByRole, render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Block from './';
